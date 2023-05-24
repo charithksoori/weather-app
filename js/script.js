b4d57d1498cf7652782a8dccea47b6f64f4dbd9a
charithksoori <116133799+charithksoori@users.noreply.github.com>
2023-05-24 23:24:38 +0530

    add a map to template

--- js/script.js
+++ js/script.js
@@ -1,3 +1,27 @@
+// Map
+const map = L.map("map").setView([51.505, -0.09], 13);
+
+L.tileLayer("https://tile.openstreetmap.org/{z}/{x}/{y}.png", {
+  attribution:
+    '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+}).addTo(map);
+
+// L.marker([51.5, -0.09])
+//   .addTo(map)
+//   .bindPopup("A pretty CSS popup.<br> Easily customizable.")
+//   .openPopup();
+
+const marker = L.marker([0, 0]).addTo(map);
+
+navigator.geolocation.getCurrentPosition(function (pos) {
+  const lat = pos.coords.latitude;
+  const long = pos.coords.longitude;
+  marker.setLatLng([lat, long]).update();
+  map.setView([lat, long], 13);
+});
+
+// ===================================================================================================================
+
 const btn = document.querySelector(".hamburger");
 const list = document.querySelector(".nav-list");
 
