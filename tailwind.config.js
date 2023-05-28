3a8d7bea8d1718e19e1618503d53fe24277633e0
charithksoori <116133799+charithksoori@users.noreply.github.com>
2023-05-28 19:18:34 +0530

    implements dark mode

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./*.html"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
+        darkGrey: "#121212",
+        midGrey: "#212121",
+        lightGrey: "#424242",
+        lowGrey: "#616161",
+        grey: "#757575",
+        textGrey: "#E0E0E0",
+        darkBlue: "",
         lightBlue: "#03a9f4",
+        testColor: "#018786",
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
