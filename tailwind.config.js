db91543474cdeba8f7acdec3ea708c6b5974e257
charithksoori <116133799+charithksoori@users.noreply.github.com>
2023-05-29 11:24:55 +0530

    add dark thme colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,10 @@ module.exports = {
         lightBlue: "#03a9f4",
         testColor: "#018786",
         hoverColor: "#03DAC6",
+        darkGreen: "#018786",
+        lightGreen: "#03DAC6",
+        darkPurple: "#3700B3",
+        lightPurple: "#6200EE",
       },
       fontFamily: {
         sans: ["Poppins", "sans-serif"],
